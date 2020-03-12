23a70ba049b0e0b546e9b495c05782e28d8cdb10
Haakon <hakon0809@github.com>
2020-03-12 17:34:23 +0100

    added base renderingSystem based on code from https://github.com/RoaringCatGames, probably changed later

--- core/src/com/mygdx/game/systems/RenderingSystem.java
+++ core/src/com/mygdx/game/systems/RenderingSystem.java
@@ -1,22 +1,99 @@
 package com.mygdx.game.systems;
 
+import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.SortedIteratingSystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.components.TextureComponent;
+import com.mygdx.game.components.TransformComponent;
 
 import java.util.Comparator;
 
 public class RenderingSystem extends SortedIteratingSystem {
-    public RenderingSystem(Family family, Comparator<Entity> comparator) {
+    //pixels for each meter
+    static final float PPM = 32.0f;
+    //we are going to have some sort of viewport
+    static final float CAMERA_WIDTH = Gdx.graphics.getWidth()/PPM;
+    static final float CAMERA_HEIGHT = Gdx.graphics.getHeight()/PPM;
+
+    public static final float PIXELS_TO_METRES = 1.0f / PPM; // get the ratio for converting pixels to metres
+
+    // convenience method to convert pixels to meters
+    public static float PixelsToMeters(float pixelValue){
+        return pixelValue * PIXELS_TO_METRES;
+    }
+    private SpriteBatch batch;
+
+    private Array<Entity> renderQueue;
+    private Comparator<Entity> comparator;
+    private OrthographicCamera cam;
+
+    //component mappers
+    private ComponentMapper<TextureComponent> textureM;
+    private ComponentMapper<TransformComponent> transformM;
+
+    public RenderingSystem(Family family, Comparator<Entity> comparator,SpriteBatch batch) {
         super(family, comparator);
+
+        this.batch = batch;
+
+        textureM = ComponentMapper.getFor(TextureComponent.class);
+        transformM = ComponentMapper.getFor(TransformComponent.class);
+
+        cam = new OrthographicCamera(CAMERA_WIDTH, CAMERA_HEIGHT);
+        cam.position.set(CAMERA_WIDTH / 2f, CAMERA_HEIGHT / 2f, 0);
     }
 
+
+    //maybe will deal with the priorityqueue of incoming renderables
     public RenderingSystem(Family family, Comparator<Entity> comparator, int priority) {
         super(family, comparator, priority);
     }
 
     @Override
-    protected void processEntity(Entity entity, float deltaTime) {
+    public void update(float deltaTime) {
+        super.update(deltaTime);
 
+        renderQueue.sort(comparator);
+        cam.update();
+        batch.setProjectionMatrix(cam.combined);
+        batch.enableBlending();
+        batch.begin();
+
+        for (Entity entity : renderQueue) {
+            TextureComponent tex = textureM.get(entity);
+            TransformComponent t = transformM.get(entity);
+
+            if (tex.region == null) {
+                continue;
+            }
+
+
+            float width = tex.region.getRegionWidth();
+            float height = tex.region.getRegionHeight();
+
+            float originX = width/2f;
+            float originY = height/2f;
+
+            batch.draw(tex.region,
+                    t.position.x - originX, t.position.y - originY,
+                    originX, originY,
+                    width, height,
+                    PixelsToMeters(t.scale.x), PixelsToMeters(t.scale.y),
+                    t.rotation);
+        }
+
+        batch.end();
+        renderQueue.clear();
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        renderQueue.add(entity);
     }
 }
