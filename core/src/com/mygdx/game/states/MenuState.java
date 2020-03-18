6ec34870f673b61ef58ffe6fc8ea5bc9555022cb
Nikolas Emil Martinsen <nikolas.emil@hotmail.com>
2020-03-18 14:45:55 +0100

    added main functions for the state

--- core/src/com/mygdx/game/states/MenuState.java
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -1,20 +1,11 @@
 package com.mygdx.game.states;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.components.StateComponent;
 
-public class MenuState extends StateManager {
-
-    protected void handleInput();
-
-
-    protected void update(float dt);
-
-
-    protected void render(SpriteBatch sb);
-
-    
-    protected void dispose();
-
+public class MenuState {
+    protected abstract void handleInput();
+    protected abstract void update(float dt);
+    protected abstract void render(SpriteBatch sb);
+    protected abstract void dispose();
 
 }
