ad43f5fae503987884cced9a56d3de634f3d13a4
Nikolas Emil Martinsen <nikolas.emil@hotmail.com>
2020-03-18 11:06:22 +0100

    added menustate

---
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.components.StateComponent;
+
+public class MenuState extends StateManager {
+
+    protected void handleInput();
+
+
+    protected void update(float dt);
+
+
+    protected void render(SpriteBatch sb);
+
+    
+    protected void dispose();
+
+
+}
