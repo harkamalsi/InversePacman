42c9ab430f5c2c53ce2d92207f0374c5d099557f
Haakon <hakon0809@github.com>
2020-03-20 15:25:07 +0100

    #20 created a playerinputsystem for handling player input

--- core/src/com/mygdx/game/components/VelocityComponent.java
+++ core/src/com/mygdx/game/components/VelocityComponent.java
@@ -6,4 +6,8 @@ import com.badlogic.gdx.math.Vector2;
 public class VelocityComponent implements Component {
     public Vector2 velocity = new Vector2(1, 1);
     public Vector2 acceleration = new Vector2();
+
+    public void setVelocity(float x, float y) {
+        velocity.set(x, y);
+    }
 }
--- core/src/com/mygdx/game/managers/EntityManager.java
+++ core/src/com/mygdx/game/managers/EntityManager.java
@@ -22,15 +22,14 @@ public class EntityManager {
     public EntityManager(Engine e, SpriteBatch batch){
         engine = e;
 
-        RenderingSystem rs = new RenderingSystem(family, comparator, batch);
+        RenderingSystem rs = new RenderingSystem(batch);
         engine.addSystem(rs);
     }
     private void createPlayerPacman(){
         // Create the player Entity and all the components that will go in the entity
         Entity entity = new Entity();
         entity.add(new VelocityComponent())
-                .add(new SpriteComponent())
-                .add(new RenderableComponent());
+                .add(new SpriteComponent());
 
         // add the entity to the engine
         engine.addEntity(entity);
