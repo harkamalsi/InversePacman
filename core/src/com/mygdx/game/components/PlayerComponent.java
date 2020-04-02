cf02486af9721bb0407759ccfeba834ec27be575
Haakon <hakon0809@github.com>
2020-04-02 12:17:02 +0200

    #20 #24 #27 Made player inputs work on pc as well as phone for a play screen. Had to change a bit on renderingsystem, texturecomponent

--- core/src/com/mygdx/game/components/PlayerComponent.java
+++ core/src/com/mygdx/game/components/PlayerComponent.java
@@ -3,24 +3,40 @@ package com.mygdx.game.components;
 import com.badlogic.ashley.core.Component;
 
 public class PlayerComponent implements Component {
-    public static final int IDLE = 0;
-    public static final int MOVE_UP = 1;
-    public static final int MOVE_DOWN = 2;
-    public static final int MOVE_LEFT = 3;
-    public static final int MOVE_RIGHT = 4;
-    public static final int HURT = 5;
-    public static final int DIE = 6;
+//    public static final int IDLE = 0;
+//    public static final int MOVE_UP = 1;
+//    public static final int MOVE_DOWN = 2;
+//    public static final int MOVE_LEFT = 3;
+//    public static final int MOVE_RIGHT = 4;
+//    public static final int HURT = 5;
+//    public static final int DIE = 6;
 
-    public int currentState;
+    private enum States {
+        IDLE,
+        MOVE_UP,
+        MOVE_DOWN,
+        MOVE_LEFT,
+        MOVE_RIGHT,
+        HURT,
+        DIE
+
+
+    }
+
+    public States currentState;
 
     public int hp;
 
     public float invincibleTimer;
 
     public PlayerComponent() {
-        currentState = IDLE;
+        currentState = States.IDLE;
         hp = 1;
         invincibleTimer = 0;
+    }
+
+    public void setCurrentState(States state) {
+        currentState = state;
     }
 
 }
