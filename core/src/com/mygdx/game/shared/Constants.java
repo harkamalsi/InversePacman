fd6b98c75d2060b9675c0ab07a2dec519267c068
harkamalsi <harkamal_guraya@hotmail.com>
2020-04-11 13:44:01 +0200

    Updatet Constants.java with new socket event types for database messages with sockets. Next it to try within network manager.

--- core/src/com/mygdx/game/shared/Constants.java
+++ core/src/com/mygdx/game/shared/Constants.java
@@ -1,7 +1,7 @@
 package com.mygdx.game.shared;
 
 public class Constants {
-    public static final String BASE_URL = "http://localhost:8080";
+    public static final String HOST = "http://localhost:8080";
 
     public static final String JOIN_LOBBY = "join_lobby";
     public static final String CREATE_LOBBY = "create_lobby";
@@ -12,9 +12,15 @@ public class Constants {
     public static final String GAME_OVER_PACMAN = "pacman_lost";
     public static final String GAME_OVER_GHOSTS = "ghosts_lost";
 
-    public static final String API_PLAYERS = "/api/players";
-    public static final String ADD_PLAYER = "/add";
-    public static final String UPDATE_PLAYER = "/update";
-    public static final String NICKNAME = "/nickname/";
-    public static final String ID = "/id/";
+    public static final String GET_ALL_PLAYERS = "get_all_players";
+    public static final String ADD_PLAYER = "add_player";
+    public static final String UPDATE_PLAYER = "update_player";
+    public static final String GET_PLAYER_WITH_NICKNAME = "get_player_with_nickname";
+    public static final String GET_PLAYER_WITH_ID = "get_player_with_id";
+
+    public static final String PLAYERS_API = "/api/players";
+    public static final String ADD_PLAYER_API = "/add";
+    public static final String UPDATE_PLAYER_API = "/update";
+    public static final String NICKNAME_API = "/nickname/";
+    public static final String ID_API = "/id/";
 }
