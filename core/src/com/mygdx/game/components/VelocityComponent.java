2f5aee37eb43b126c68f701c23975c3774a0f572
Karl <kmlyso@gmail.com>
2020-04-21 14:08:33 +0200

    again to merge with dev

--- core/src/com/mygdx/game/components/VelocityComponent.java
+++ core/src/com/mygdx/game/components/VelocityComponent.java
@@ -4,11 +4,7 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Vector2;
 
 public class VelocityComponent implements Component {
-<<<<<<< HEAD
-    public Vector2 velocity = new Vector2(0, 0);
-=======
     public Vector2 velocity = new Vector2(2.5f, 2.5f);
->>>>>>> safe_dev_2
     public Vector2 acceleration = new Vector2(0,0);
 
     public void setVelocity(float x, float y) {
