57660d4495189dc11d5dd3542d07e703611a5c6c
Nikolas Emil Martinsen <nikolas.emil@hotmail.com>
2020-04-26 10:06:26 +0200

    fixed hud on playscreen to look cooler and rezised the buttons

--- core/src/com/mygdx/game/screens/play/PlayScreen.java
+++ core/src/com/mygdx/game/screens/play/PlayScreen.java
@@ -91,8 +91,12 @@ public final class PlayScreen extends AbstractScreen {
     private TextureRegion pausescreen;
     private TextureRegion pausetexture;
     private TextureRegion back;
+    private TextureRegion ellipse;
+
 
     private Sprite pauseSprite;
+    private Sprite ellipseSprite;
+
 
     private Sprite pauseButtonSprite;
     private Sprite pacmanSpritus;
@@ -105,6 +109,8 @@ public final class PlayScreen extends AbstractScreen {
     private Entity backButton;
     private Entity ghost;
     private Entity pill;
+    private Entity ellipseEntity;
+
 
 
     public static boolean MULTIPLAYER;
@@ -151,8 +157,9 @@ public final class PlayScreen extends AbstractScreen {
         super(app, engine);
 
         this.engine = engine;
-        pausetexture = new TextureRegion(new Texture("playscreen/pause3x.png"));
-        back = new TextureRegion(new Texture("back3x.png"));
+        ellipse = new TextureRegion(new Texture("menuscreen/ellipse_color_change_correct.png"));
+        pausetexture = new TextureRegion(new Texture("playscreen/pause2x.png"));
+        back = new TextureRegion(new Texture("playscreen/back2x.png"));
 
 //        this.engine = engine;
 //         Sets the camera; width and height.
@@ -329,6 +336,10 @@ public final class PlayScreen extends AbstractScreen {
         littlePillSprite = new Texture("white_pill.png");
         powerPillSprite = new Texture("orange_pill.png");
 
+        ellipseSprite = new Sprite(ellipse);
+        ellipseEntity = new Entity();
+        app.addSpriteEntity(ellipseSprite, ellipseEntity, engine, (Gdx.graphics.getWidth() / 2 - (ellipse.getRegionWidth() / 2 * (scaleX))), (Gdx.graphics.getHeight() - (ellipse.getRegionHeight() * (scaleY))), (ellipse.getRegionWidth() * (scaleX)), (ellipse.getRegionHeight() * (scaleY)), false, true, true, false);
+
         Vector2 scale = new Vector2();
         for (int i = 0; i < WorldBuilder.getPillList().size(); i++) {
             PillComponent pillComponent = WorldBuilder.getPillList().get(i);
@@ -433,11 +444,11 @@ public final class PlayScreen extends AbstractScreen {
 
         pauseButtonSprite = new Sprite(pausetexture);
         pauseButton = new Entity();
-        app.addSpriteEntity(pauseButtonSprite, pauseButton, engine,  Gdx.graphics.getWidth() / 1.5f, 50 * 32 * scaleX/ 1.5f, pauseButtonSprite.getRegionWidth() * scaleX, pauseButtonSprite.getRegionHeight() * scaleX, true,false, false, false);
+        app.addSpriteEntity(pauseButtonSprite, pauseButton, engine,  Gdx.graphics.getWidth() - (pauseButtonSprite.getRegionWidth() * scaleX), 50 * 32 * scaleX/ 1.5f, pauseButtonSprite.getRegionWidth() * scaleX, pauseButtonSprite.getRegionHeight() * scaleX, true,false, false, false);
 
         backSprite = new Sprite(back);
         backButton = new Entity();
-        app.addSpriteEntity(backSprite, backButton, engine,  Gdx.graphics.getWidth() / 10, 50 * 32 * scaleX/ 1.5f, pauseButtonSprite.getRegionWidth() * scaleX, pauseButtonSprite.getRegionHeight() * scaleX, true,false, false, false);
+        app.addSpriteEntity(backSprite, backButton, engine, 0, 50 * 32 * scaleX/ 1.5f, backSprite.getRegionWidth() * scaleX, backSprite.getRegionHeight() * scaleX, true,false, false, false);
 
         musicEntity = new Entity();
         musicEntity.add(new MusicComponent(Gdx.files.internal("music/play")));
