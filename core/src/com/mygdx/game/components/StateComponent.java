8a8d0b19822a2ddc930bad2542038c7ec68dce02
Haakon <hakon0809@github.com>
2020-04-27 22:48:31 +0200

    removed unused variables from state component

--- core/src/com/mygdx/game/components/StateComponent.java
+++ core/src/com/mygdx/game/components/StateComponent.java
@@ -7,17 +7,11 @@ public class StateComponent implements Component {
 
     public int currentState;
 
-    public int hp;
-
-    public float invincibleTimer;
-
     public float stateTime;
 
     public StateComponent(int state){
         this.currentState = state;
         stateTime = 0.0f;
-        hp = 1;
-        invincibleTimer = 0;
     }
 
     public int getState(){
